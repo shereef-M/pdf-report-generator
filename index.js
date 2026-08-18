8ac666cfe304e1e0972517d914b43c7e58d3daaa
Abubakar Muhammad Sherif <abubakarsherrif2@gmail.com>
2026-08-18 22:44:43 +0100

    Stage 0: setup ready

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
