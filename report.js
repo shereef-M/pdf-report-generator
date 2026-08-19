c9bc8bb645dad358ceeb3c9f205e8d60d05c8fe9
Abubakar Muhammad Sherif <abubakarsherrif2@gmail.com>
2026-08-19 10:05:54 +0100

    Stage 2: aggregation queries

---
+++ report.js
@@ -0,0 +1,28 @@
+const { DatabaseSync } = require("node:sqlite");
+
+function getReportData() {
+  const db = new DatabaseSync("report.db");
+
+  const totalBooks = db.prepare("SELECT COUNT(*) as count FROM books").get();
+
+  const averagePrice = db.prepare("SELECT AVG(price) as avg FROM books").get();
+
+  const topExpensive = db
+    .prepare("SELECT title, price FROM books ORDER BY price DESC LIMIT 5")
+    .all();
+
+  const byRating = db
+    .prepare("SELECT rating, COUNT(*) as count FROM books GROUP BY rating")
+    .all();
+
+  db.close();
+
+  return {
+    totalBooks: totalBooks.count,
+    averagePrice: averagePrice.avg,
+    topExpensive,
+    byRating,
+  };
+}
+
+module.exports = { getReportData };
---
+++ test-report.js
@@ -0,0 +1,2 @@
+const {getReportData} = require('./report')
+console.log(JSON.stringify(getReportData(), null,2))
